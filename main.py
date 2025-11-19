249f5cd5645cef17da17d926dde695f61f7be679
“angel” <jangeltr@hotmail.com>
2025-11-19 06:49:05 -0600

    Primer commit funcionando

---
+++ main.py
@@ -0,0 +1,15 @@
+# pip install -qU "langchain[anthropic]" to call the model
+
+from langchain.agents import create_agent
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    #model="claude-sonnet-4-5-20250929",
+    model="gpt-4o-mini",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
